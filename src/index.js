5ff1793e1c20e85c07841205352f1ff033c5c944
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-07-07 18:25:40 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+//make modules for each of the things you want to implement in ur code, suc as the taskbar with Home, Menu and Content
+//then appendchild the module Home when clicking and rmvchld menu and content to let em dissapear and appear!
+//not to hard just alot of organising lol.
+
 console.log('test');
+
+console.log('another test bby');
+
+//works when doing npx webpack then it runs it to the main.js
